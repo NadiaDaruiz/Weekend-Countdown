e5992f26bfb75ad7a78985ba924e06f24a71bb8f
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-02-20 22:01:35 +0100

    add daysLeft to weekend

--- script.js
+++ script.js
@@ -11,17 +11,20 @@
     button.addEventListener('click', getName)
 
     function getName() {
-        let name = input.value.toUpperCase().substring(0, 1) + input.value.toLowerCase().substring(1)
-        let p = document.createElement('p')
+        let name = input.value.toUpperCase().substring(0, 1) + input.value.toLowerCase().substring(1);
+        let p = document.createElement('p');
 
         // figure out the date thing
-        let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-        let date = weekdays[new Date().getDay()]
-        console.log(date)
 
-        let message = `Hello ${name}. Today is ${date}. And it's x till weekend.`
+        let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let date = weekdays[new Date().getDay()];
+        let daysLeft = 5 - weekdays.indexOf(date); // 5 cause is weekdays, I'm leaving out Sat and Sun
+
+
+        let message = `Hello ${name}. Today is ${date}, only ${daysLeft} day till the weekend.`
         p.innerHTML = message;
 
         messageBox.appendChild(p);
     }
 })()
+
