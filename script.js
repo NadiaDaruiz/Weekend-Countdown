f191d7061544896e9df78761318c4a3fe553071d
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-02-21 15:07:27 +0100

    add if statment to js

--- script.js
+++ script.js
@@ -7,24 +7,28 @@
     const input = document.getElementById('name')
     const button = document.querySelector('.btn')
     const messageBox = document.querySelector('.message__box')
+    const p = document.querySelector('p')
 
     button.addEventListener('click', getName)
 
     function getName() {
         let name = input.value.toUpperCase().substring(0, 1) + input.value.toLowerCase().substring(1);
-        let p = document.createElement('p');
 
         // figure out the date thing
 
         let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         let date = weekdays[new Date().getDay()];
-        let daysLeft = 5 - weekdays.indexOf(date); // 5 cause is weekdays, I'm leaving out Sat and Sun
+        let daysLeft = 6 - weekdays.indexOf(date); // 6 cause is weekdays, I'm leaving out Sat and Sun
 
+        let message = '';
 
-        let message = `Hello ${name}. Today is ${date}, only ${daysLeft} day till the weekend.`
+        if (date === 'Saturday' || date === 'Sunday') {
+            message = `Hello ${name}.Today is ${date}. It's the weekend!`
+        } else {
+            message = `Hello ${name}.Today is ${date}, only ${daysLeft} day till the weekend.`
+        }
+
         p.innerHTML = message;
-
-        messageBox.appendChild(p);
     }
 })()
 
